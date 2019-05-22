b8a152c5e6745f21b4b372531513d2719c1462e5
Blake Lazarine <blake.l.lazarine@gmail.com>
2019-05-22 10:09:13 -0700

    v2.4 commented code, made/improved universal claibration method - taken from Moving Mosaic distribution of library

--- ODrive_Ease_Lib.py
+++ ODrive_Ease_Lib.py
@@ -3,8 +3,8 @@ from odrive.enums import *
 import time
 import usb.core
 
-# Used to make using the ODrive easier Version 2.3
-# Last update April 23, 2019 by Blake Lazarine
+# Used to make using the ODrive easier Version 2.4
+# Last update May 22, 2019 by Blake Lazarine
 
 def find_ODrives():
     dev = usb.core.find(find_all=1, idVendor=0x1209, idProduct=0x0d32)
@@ -26,6 +26,7 @@ class ODrive_Axis(object):
         self.axis.controller.config.vel_limit = vel_lim
         self.busy_lim = 500
 
+	# enters full calibration sequence
     def calibrate(self):
         self.axis.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
         start = time.time()
@@ -35,6 +36,7 @@ class ODrive_Axis(object):
                 print("could not calibrate, try rebooting odrive")
                 return False
 
+	#enters encoder offset calibration
     def calibrate_encoder(self):
         self.axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
         start = time.time()
@@ -44,73 +46,98 @@ class ODrive_Axis(object):
                 print("could not calibrate, try rebooting odrive")
                 return False
 
+	#checks if the motor is claibrated and the encoder is ready
     def is_calibrated(self):
-        return self.axis.motor.is_calibrated
+        return self.axis.motor.is_calibrated and self.axis.encoder.is_ready
 
+	#sets the motor to a specified velocity. Does not go over the velocity limit
     def set_vel(self, vel):
         self.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
         self.axis.controller.vel_setpoint = vel
 
+	#sets the motor's velocity limit. Default is 20000
     def set_vel_limit(self, vel):
         self.axis.controller.config.vel_limit = vel
 
+	#returns the velocity limit
     def get_vel_limit(self):
         return self.axis.controller.config.vel_limit
     
+	#sets the zero (home pos) to the specified position
     def set_zero(self, pos):
         self.zero = pos
+		
+	#sets the zero to the current_position
+	def set_home(self):
+		self.zero = self.get_raw_pos()
 
+    #returns the current position relative to the home
     def get_pos(self):
         return self.axis.encoder.pos_estimate - self.zero
 
+    #returns the current position directly from the encoder
     def get_raw_pos(self):
         return self.axis.encoder.pos_estimate
 
+	#sets the desired position
     def set_pos(self, pos):
         desired_pos = pos + self.zero
         self.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
         self.axis.controller.pos_setpoint = desired_pos
 
+    #sets position using the trajectory control mode
     def set_pos_trap(self, pos):
         desired_pos = pos + self.zero
         self.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis.controller.config.control_mode = CTRL_MODE_TRAJECTORY_CONTROL
         self.axis.controller.move_to_pos(desired_pos)
 
+	#sets the current limit
     def set_curr_limit(self, val):
         self.axis.motor.config.current_lim = val
-
+   
+    #returns the current limit
     def get_curr_limit(self):
         return self.axis.motor.config.current_lim
-    
+
+    #sets the current sent to the motor    
     def set_current(self, curr):
         self.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis.controller.config.control_mode = CTRL_MODE_CURRENT_CONTROL
         self.axis.controller.current_setpoint = curr
 
+    #returns the velocity measured from the encoder
     def get_vel(self):
         return self.axis.encoder.vel_estimate
 
+    #Sets the position gain value
     def set_pos_gain(self, val):
         self.axis.controller.config.pos_gain = val
 
+    #returns the position gain value
     def get_pos_gain(self):
         return self.axis.controller.config.pos_gain
 
+    #sets the velocity proportional gain value
     def set_vel_gain(self, val):
         self.axis.controller.config.vel_gain = val
 
+    #returns the velocity proportional gain value
     def get_vel_gain(self):
         return self.axis.controller.config.vel_gain
 
+    #sets the velocity integrator gain value. Usually this is 0
     def set_vel_integrator_gain(self, val):
         self.axis.controller.config.vel_integrator_gain = val
 
+    #returns the velocity integrator gain value. Usually this is 0
     def get_vel_integrator_gain(self):
         return self.axis.controller.config.vel_integrator_gain
 
+    #checks if the motor is moving. Need to use a threshold speed. by default it is 500 counts/second
+	#this can be set during initialization of ODrive_Axis object, through later setting of the busy_lim, or through a parameter to the is_busy method
     def is_busy(self, speed=-1):
         if speed == -1:
             speed = self.busy_lim
@@ -119,9 +146,11 @@ class ODrive_Axis(object):
         else:
             return False
 
+    #sets the current allowed during the calibration sequence. Higher currents are needed when the motor encounters more resistence to motion
     def set_calibration_current(self, curr):
         self.axis.motor.config.calibration_current = curr
 
+    #returns the allowed calibraiton current. By default it is 5 amps (3 phase not DC)
     def get_calibration_current(self):
         return self.axis.motor.config.calibration_current
 
@@ -161,6 +190,9 @@ class ODrive_Axis(object):
         print('ODrive homed correctly')
         return True
 
+    #homes the motor by having it move towards one side with a constant velocity. Once it can no longer move, it considers this its home
+	#The direction can be specified either by the sign of the velocty passed in or through the direction parameter
+	#If the length of the track is known, it can be passed in. If this is done, after moving to one side, the motor will move to the other to find if the homing was successful.
     def home_with_vel(self, vel, length=-1, direction=1):
         self.set_vel(vel * -1 * direction)
         print('here')
@@ -275,7 +307,35 @@ class double_ODrive(object):
         self.y.axis.min_endstop.confid.enabled = False
         self.y.axis.max_endstop.config.enabled = False
 
-print('ODrive Ease Lib 2.3')
+#calibrates a list of ODrive_Axis objects the minimal amount
+def calibrate_list(odrives):
+    calibrated = []
+	i = 0
+    for o in odrives:
+		calibrated.append(False)
+        if o.axis.motor.is_calibrated:
+            if o.axis.encoder.is_ready:
+                calibrated[-1] = True
+            else:
+                o.axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+        else:
+            o.axis.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    is_done = False
+	
+    while not is_done:
+	    i = -1
+        for o in odrives:
+		    i+=1
+            if o.axis.current_state == AXIS_STATE_IDLE or calibrated[i] == True:
+                calibrated[i] = True
+				continue
+            else:
+                time.sleep(0.2)
+                break
+        else:
+            is_done = True
+
+print('ODrive Ease Lib 2.4')
 '''
 odrv0 = odrive.find_any()
 print(str(odrv0.vbus_voltage))
