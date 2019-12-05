8d3d0081eae5ab64ddbcfde479a7f034be76c8aa
dylan <dylanbien@gmail.com>
2019-12-05 13:54:10 -0800

    motor calibration

---
+++ motor_calibration.py
@@ -0,0 +1,35 @@
+import time
+
+import odrive
+import usb.core
+from odrive.enums import *
+
+calibrating = 0
+
+od = odrive.find_any()
+
+print('odrive found: ' + od.serial_number)
+time.sleep(2)
+
+if calibrating == 0:
+    od.axis0.requested_state.AXIS_STATE_MOTOR_CALIBRATION
+if calibrating == 1:
+    od.axis1.requested_state.AXIS_STATE_MOTOR_CALIBRATION
+
+print('motor calibrated')
+
+time.sleep(2)
+if calibrating == 0:
+    od.axis0.motor.config.pre_calibrated = True
+if calibrating == 1:
+    od.axis1.motor.config.pre_calibrated = True
+
+od.save_configuration()
+
+print('configuration saved')
+time.sleep(2)
+
+od.reboot()
+
+print('motor calibration complete')
+
