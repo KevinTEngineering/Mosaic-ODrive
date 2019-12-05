b285929b8f2b4ad7f9e850656d5b3f9e7d5093b8
Wade <wadevaresio@gmail.com>
2019-12-05 14:38:00 -0800

    Revert "Refactored motor calibration"
    This reverts commit c201ac17384be73d4a2f638e1726235308236b48.

--- motor_calibration.py
+++ motor_calibration.py
@@ -1,52 +1,48 @@
-import odrive
-from fibre.protocol import ChannelBrokenException
-from odrive.enums import *
+import time
 
+import odrive
+import usb.core
+from odrive.enums import *
+from fibre.protocol import ChannelBrokenException
 
 def reboot_odrive():
-    """
-    Save the configuration and reboot the ODrive.
-    """
     try:
-        print("Rebooting ODrive")
         od.save_configuration()
         od.reboot()
     except ChannelBrokenException:
         print('motor calibration complete')
         exit(0)
 
+calibrating = 0
 
-def calibrate_motor(axis):
-    """
-    Calibrate the ODrive motor
-    @param axis: The axis number the ODrive is connected to
-    """
-    global od
-    od = odrive.find_any()
+od = odrive.find_any()
 
-    print("odrive found: " + str(od.serial_number))
+print("odrive found: " + str(od.serial_number))
+time.sleep(2)
 
-    if od.axis0.motor.config.pre_calibrated:
-        print("The motor is already calibrated, setting pre_calibrated to false. Please rerun this script")
-        od.axis0.motor.config.pre_calibrated = False
-        reboot_odrive()
-
-    if axis :
-        od.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-    else:
-        od.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-
-    print('motor calibrated')
-
-    if axis:
-        od.axis1.motor.config.pre_calibrated = True
-    else:
-        od.axis0.motor.config.pre_calibrated = True
-
-    print('configuration saved')
-
-
-if __name__ == "__main__":
-    print("Enter axis number")
-    calibrate_motor(int(input()))
+if od.axis0.motor.config.pre_calibrated:
+    print("The motor is already calibrated, setting pre_calibrated to false. Please rerun this script")
+    od.axis0.motor.config.pre_calibrated = False
     reboot_odrive()
+
+if calibrating == 0:
+    od.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+if calibrating == 1:
+    od.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+
+print('motor calibrated')
+
+time.sleep(2)
+if calibrating == 0:
+    od.axis0.motor.config.pre_calibrated = True
+if calibrating == 1:
+    od.axis1.motor.config.pre_calibrated = True
+
+
+print('configuration saved')
+time.sleep(2)
+
+reboot_odrive()
+
+
+
