bc932c42b42a111e8d173df22e54556f3e317ec3
gabimcclure <gabimcclure@dpengineering.org>
2019-12-05 14:20:04 -0800

    work on motor calibration

--- motor_calibration.py
+++ motor_calibration.py
@@ -3,18 +3,32 @@ import time
 import odrive
 import usb.core
 from odrive.enums import *
+from fibre.protocol import ChannelBrokenException
+
+def reboot_odrive():
+    try:
+        od.save_configuration()
+        od.reboot()
+    except ChannelBrokenException:
+        print('motor calibration complete')
+        exit(0)
 
 calibrating = 0
 
 od = odrive.find_any()
 
-print('odrive found: ' + od.serial_number)
+print("odrive found: " + str(od.serial_number))
 time.sleep(2)
 
+if od.axis0.motor.config.pre_calibrated:
+    print("The motor is already calibrated, setting pre_calibrated to false. Please rerun this script")
+    od.axis0.motor.config.pre_calibrated = False
+    reboot_odrive()
+
 if calibrating == 0:
-    od.axis0.requested_state.AXIS_STATE_MOTOR_CALIBRATION
+    od.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
 if calibrating == 1:
-    od.axis1.requested_state.AXIS_STATE_MOTOR_CALIBRATION
+    od.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
 
 print('motor calibrated')
 
@@ -24,12 +38,11 @@ if calibrating == 0:
 if calibrating == 1:
     od.axis1.motor.config.pre_calibrated = True
 
-od.save_configuration()
 
 print('configuration saved')
 time.sleep(2)
 
-od.reboot()
+reboot_odrive()
 
-print('motor calibration complete')
+
 
